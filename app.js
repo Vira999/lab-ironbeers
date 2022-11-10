5ac6bac791d13b59989556369b59b9293989d3a8
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-11-10 18:16:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,39 +7,42 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
     .then(beersFromApi => {
-      res.render('beers.hbs', { beersFromApi });
+      console.log('Beers from the database: ', beersFromApi)
+        res.render('beers', {allBeers: beersFromApi});
     })
-    .catch(error => console.log(error));
+  .catch(error => console.log(error));
+
 });
 
-app.get('/randombeers', (req, res) => {
+app.get('/ranombeers', (req, res) => {
   punkAPI
     .getRandom()
-    .then(randomBeer => {
-      let beer = randomBeer[0];
-      res.render('randombeers.hbs', beer);
-      // console.log(beer);
+    .then(responseFromAPI => {
+      console.log('Beers from the database: ', responseFromAPI)
+    res.render('randombeers',{randomBeers:responseFromAPI});
     })
-
     .catch(error => console.log(error));
-});
-
+  
+  });
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-  
+
 
 // ...
 
